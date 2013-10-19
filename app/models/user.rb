55a4c8975d293c7e323383d65a3f53b97418d7b5
Paulo Ragonha <paulo@ragonha.me>
2013-10-19 17:41:41 -0300

    adds methods to get only the good or bad metrics of a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User
   field :twitter_id, type: String
   field :twitter_screen_name, type: String
   field :twitter_picture_url, type: String
+
+  def good_metrics
+    metrics.select { |key, value| value > 0 }
+  end
+
+  def bad_metrics
+    metrics.select { |key, value| value < 0 }
+  end
 end
