cbaf8156594534d436f2cfc6c52c391deda2a325
Paulo Ragonha <paulo@ragonha.me>
2013-10-20 16:17:19 -0200

    capistrano recipe to start sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,26 @@
+require 'sidekiq/capistrano'
+
 set :application, 'should you follow me'
 set :repo_url, 'git@github.com:railsrumble/r13-team-518.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
 set :deploy_to, '/www/shouldyoufollowme'
-# set :scm, :git
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
+SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :sidekiq_cmd,           ->{ :sidekiq }
+set :sidekiqctl_cmd,        ->{ :sidekiqctl }
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+                              # must be relative to Rails.root. If this changes, you'll need to manually
+                              # stop the existing sidekiq process.
+set :sidekiq_pid,           ->{ "tmp/sidekiq.pid" }
+
+                              # "-d -i INT -P PATH" are added automatically.
+set :sidekiq_options,       ->{ "-e #{fetch(:rails_env, 'production')} -L #{current_path}/log/sidekiq.log" }
+
+set :sidekiq_timeout,       ->{ 10 }
+set :sidekiq_role,          ->{ :app }
+set :sidekiq_processes,     ->{ 1 }
 
 namespace :deploy do
   task :restart do
@@ -26,27 +32,3 @@ namespace :deploy do
 
   after :finishing, 'deploy:cleanup'
 end
-
-
-# namespace :deploy do
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       # execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-#   after :finishing, 'deploy:cleanup'
-
-# end
