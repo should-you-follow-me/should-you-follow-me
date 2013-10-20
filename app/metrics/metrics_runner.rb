c0ce8ece6e74be8a744d360ca17e70872986f6b7
DanilloCorvalan <danillo.corvalan@gmail.com>
2013-10-20 14:29:24 -0200

    metrics runner added - missing some tests

---
+++ app/metrics/metrics_runner.rb
@@ -0,0 +1,18 @@
+class MetricRunner
+  def run(tweets)
+    @scores = {}
+
+    Metrics.submodules.each do |sub_module|
+      score = sub_module.run(tweets)
+      @scores.merge!(score) unless score.nil?
+    end
+
+    return @scores
+  end
+end
+
+class Module
+  def submodules
+    constants.collect {|const_name| const_get(const_name)}.select {|const| const.class == Module}
+  end
+end
