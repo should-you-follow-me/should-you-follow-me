17ff21523b187a7c51c937d727624ee097f46aa0
laurocaetano <laurocaetano1@gmail.com>
2013-10-20 17:30:34 -0200

    Use single quotes

--- spec/metrics/flooder_spec.rb
+++ spec/metrics/flooder_spec.rb
@@ -4,11 +4,11 @@ require 'flooder'
 describe 'Floader Metric' do
    it 'Should get score of -10 when tweets more than five times per day' do
     tweets = [
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 12:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 11:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 10:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 09:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 08:59:01 +0000 2007")
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 12:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 11:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 10:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 09:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 08:59:01 +0000 2007')
     ]
 
     results = Metrics::FlooderMetric.run tweets
@@ -17,11 +17,11 @@ describe 'Floader Metric' do
 
   it 'Should not get flooder metric score when tweets regularly' do
     tweets = [
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 12:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 11:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Mon Jul 16 08:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Sun Jul 15 10:59:01 +0000 2007"),
-      Twitter::Tweet.new(id: 1, created_at: "Sun Jul 15 09:59:01 +0000 2007")      
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 12:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 11:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Mon Jul 16 08:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Sun Jul 15 10:59:01 +0000 2007'),
+      Twitter::Tweet.new(id: 1, created_at: 'Sun Jul 15 09:59:01 +0000 2007')
     ]
 
     results = Metrics::FlooderMetric.run tweets
