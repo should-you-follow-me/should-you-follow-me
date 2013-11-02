66cf7b9bd1764707e29623b086bc07f7f6ea45f4
Lauro Caetano <laurocaetano1@gmail.com>
2013-11-02 16:01:37 -0200

    :nail_care:

--- spec/metrics/metrics_runner_spec.rb
+++ spec/metrics/metrics_runner_spec.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
 require 'metrics_runner'
 
-describe 'Metrics Runner' do  
-  before(:each) do
-    @metrics_runner = MetricsRunner.new()
-  end
+describe 'Metrics Runner' do
+  let(:metrics_runner) { MetricsRunner.new }
 
   it 'Should get empty result when do not have any metric matching' do
     tweets = [
-      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: 1 }, { id: 2 }]})
+      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: 1 }, { id: 2 }] })
     ]
 
-    results = @metrics_runner.run tweets
+    results = metrics_runner.run tweets
 
     expect(results).to be_empty
   end
 
   it 'Should return bieber and astrophysics scores' do
     tweets = [
-      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: Metrics::BieberUserMention::USER_ID}]}),
-      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: Metrics::AstrophysicsUserMention::USER_IDS[0]}]}),
-      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: 1 }, { id: 2 }]})
+      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: Metrics::BieberUserMention::USER_ID }] }),
+      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: Metrics::AstrophysicsUserMention::USER_IDS[0] }] }),
+      Twitter::Tweet.new(id: 1, entities: { user_mentions: [{ id: 1 }, { id: 2 }] })
     ]
 
-    results = @metrics_runner.run tweets
+    results = metrics_runner.run tweets
+
     expect(results).to eql({ bieber: -10, astrophysics: 10 })
   end
-
-end 
+end
